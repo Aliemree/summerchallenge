dd83623bac73e1060f77a93907105c0e6f082d10
Ali Emre <ali.emree.tr@gmail.com>
2023-07-23 14:07:49 +0300

    First code Hard Skill

---
+++ SummerChallenge/Challange1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace YasHesaplama
+{
+    class YasHesaplama
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Doğum tarihinizi (yyyy-aa-gg formatında) giriniz:");
+        string dogumTarihiStr = Console.ReadLine();
+
+        // Doğum tarihini 'yyyy-aa-gg' formatından DateTime nesnesine dönüştür
+        DateTime dogumTarihi = DateTime.ParseExact(dogumTarihiStr, "yyyy-MM-dd", null);
+
+        // Şu anki tarihi al
+        DateTime suAn = DateTime.Now;
+
+        // Yaşı hesapla
+        int yas = suAn.Year - dogumTarihi.Year;
+
+        // Doğum günü henüz gelmemişse bir yaş düşür
+        if (suAn.Month < dogumTarihi.Month || (suAn.Month == dogumTarihi.Month && suAn.Day < dogumTarihi.Day))
+        {
+            yas--;
+        }
+
+        Console.WriteLine("Yaşınız: " + yas);
+        }
+    }
+}
