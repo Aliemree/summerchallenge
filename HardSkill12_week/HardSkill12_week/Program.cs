533561d1d5f239154089f05b3540cfc9839b4cff
Ali Emre <ali.emree.tr@gmail.com>
2023-08-21 18:46:55 +0300

    Hard skill 12. hafta görevi tamamlandı

---
+++ HardSkill12_week/HardSkill12_week/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PoolFillingTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // İlk su girişi hızı: 1/10 (havuz/saat)
+            // İkinci su girişi hızı: 1/15 (havuz/saat)
+            // Havuzun boşalma hızı: -1/30 (havuz/saat)
+
+            // İki su girişi aynı anda açıldığında toplam su girişi hızı:
+            double totalInputRate = 1.0 / 10 + 1.0 / 15;
+
+            // Havuzun dolma süresi hesaplanıyor (1 saat / toplam giriş hızı):
+            double fillingTime = 1.0 / totalInputRate;
+
+            Console.WriteLine($"Havuzun dolma süresi: {fillingTime:F2} saat");
+        }
+    }
+}
