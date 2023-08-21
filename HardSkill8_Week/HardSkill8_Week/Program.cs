6b09bdedd14d23d5e3b216482cae26d63bc66a4e
Ali Emre <ali.emree.tr@gmail.com>
2023-08-21 18:41:24 +0300

    Hard Skill 8. Hafta görevi

---
+++ HardSkill8_Week/HardSkill8_Week/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DuplicationCheck
+{
+    class Program
+    {
+        // Fonksiyon: Verilen metinde ikileme olup olmadığını kontrol eder.
+        static bool CheckForDuplication(string text)
+        {
+            string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                if (i + 1 < words.Length && words[i] == words[i + 1])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Bir cümle girin: ");
+            string sentence = Console.ReadLine();
+
+            if (CheckForDuplication(sentence))
+            {
+                Console.WriteLine("Cümlede ikileme bulundu.");
+            }
+            else
+            {
+                Console.WriteLine("Cümlede ikileme bulunamadı.");
+            }
+        }
+    }
+}
