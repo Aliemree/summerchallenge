190276330400b2035fc699bb9442d327edb991e4
Ali Emre <ali.emree.tr@gmail.com>
2023-08-21 18:43:27 +0300

    hard skill 10. hafta görevi

---
+++ HardSkill10_week/HardSkill10_week/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DivisorSumCalculator
+{
+    class Program
+    {
+        // Fonksiyon: Bir sayının tam bölenlerinin toplamını hesaplar.
+        static int CalculateDivisorSum(int number)
+        {
+            int divisorSum = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    divisorSum += i;
+                }
+            }
+
+            return divisorSum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı girin: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int sumOfDivisors = CalculateDivisorSum(number);
+
+            Console.WriteLine($"{number} sayısının tam bölenlerinin toplamı: {sumOfDivisors}");
+        }
+    }
+}
