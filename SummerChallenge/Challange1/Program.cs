8c8af654ed61dbfdc5fd1e54259537d1568e4ca0
Ali Emre <ali.emree.tr@gmail.com>
2023-08-21 18:26:34 +0300

    Hard skill görevi tamamlandı. 4hafta hard skill

--- SummerChallenge/Challange1/Program.cs
+++ SummerChallenge/Challange1/Program.cs
@@ -2,57 +2,82 @@
 
 namespace SummerChallange
     
-{
-    class YasHesaplama
-{
-    static void Main(string[] args)
+{ class KaraKök
     {
-        Console.WriteLine("Doğum tarihinizi (yyyy-aa-gg formatında) giriniz:");
-        string dogumTarihiStr = Console.ReadLine();
-
-        // Doğum tarihini 'yyyy-aa-gg' formatından DateTime nesnesine dönüştür
-        DateTime dogumTarihi = DateTime.ParseExact(dogumTarihiStr, "yyyy-MM-dd", null);
-
-        // Şu anki tarihi al
-        DateTime suAn = DateTime.Now;
-
-        // Yaşı hesapla
-        int yas = suAn.Year - dogumTarihi.Year;
-
-        // Doğum günü henüz gelmemişse bir yaş düşür
-        if (suAn.Month < dogumTarihi.Month || (suAn.Month == dogumTarihi.Month && suAn.Day < dogumTarihi.Day))
-        {
-            yas--;
-        }
-
-        Console.WriteLine("Yaşınız: " + yas);
-        }
-    }
-
-
-    class CostAndPrice
-    {
-
         static void Main(string[] args)
         {
-            // Birim maliyet ve birim satış fiyatı değişkenlerini tanımla
-            double cost = 100;
-            double price = 150;
+            Console.Write("Kullanıcı Sayı: ");
+            double userInput = Convert.ToDouble(Console.ReadLine());
 
-            // Birim maliyetin birim satış fiyatından düşük olduğunu kontrol et
-            if (cost >= price)
+            double squareRoot = Math.Sqrt(userInput);
+
+            if (Math.Abs(Math.Floor(squareRoot + 0.5) - squareRoot) < double.Epsilon)
             {
-                Console.WriteLine("Hata: Birim maliyet birim satış fiyatından düşük olmalıdır!");
+                Console.WriteLine($"Cevap: {squareRoot}");
             }
             else
             {
-                // Kâr edilmeye başlamak için gereken adet sayısını hesapla
-                int adet = (int)Math.Ceiling(cost / (price - cost));
-
-                Console.WriteLine($"Birim Maliyet: {cost} TL");
-                Console.WriteLine($"Birim Satış Fiyatı: {price} TL");
-                Console.WriteLine($"Kâr Edilmeye Başlanacak Adet: {adet}");
+                Console.WriteLine("Cevap: Karekökten tam olarak çıkmıyor.");
             }
         }
     }
+
+
+
+
+
+
+
+//    class YasHesaplama
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Doğum tarihinizi (yyyy-aa-gg formatında) giriniz:");
+//        string dogumTarihiStr = Console.ReadLine();
+
+//        // Doğum tarihini 'yyyy-aa-gg' formatından DateTime nesnesine dönüştür
+//        DateTime dogumTarihi = DateTime.ParseExact(dogumTarihiStr, "yyyy-MM-dd", null);
+
+//        // Şu anki tarihi al
+//        DateTime suAn = DateTime.Now;
+
+//        // Yaşı hesapla
+//        int yas = suAn.Year - dogumTarihi.Year;
+
+//        // Doğum günü henüz gelmemişse bir yaş düşür
+//        if (suAn.Month < dogumTarihi.Month || (suAn.Month == dogumTarihi.Month && suAn.Day < dogumTarihi.Day))
+//        {
+//            yas--;
+//        }
+
+//        Console.WriteLine("Yaşınız: " + yas);
+//        }
+//    }
+
+
+    //class CostAndPrice
+    //{
+
+    //    static void Main(string[] args)
+    //    {
+    //        // Birim maliyet ve birim satış fiyatı değişkenlerini tanımla
+    //        double cost = 100;
+    //        double price = 150;
+
+    //        // Birim maliyetin birim satış fiyatından düşük olduğunu kontrol et
+    //        if (cost >= price)
+    //        {
+    //            Console.WriteLine("Hata: Birim maliyet birim satış fiyatından düşük olmalıdır!");
+    //        }
+    //        else
+    //        {
+    //            // Kâr edilmeye başlamak için gereken adet sayısını hesapla
+    //            int adet = (int)Math.Ceiling(cost / (price - cost));
+
+    //            Console.WriteLine($"Birim Maliyet: {cost} TL");
+    //            Console.WriteLine($"Birim Satış Fiyatı: {price} TL");
+    //            Console.WriteLine($"Kâr Edilmeye Başlanacak Adet: {adet}");
+    //        }
+    //    }
+    //}
 }
