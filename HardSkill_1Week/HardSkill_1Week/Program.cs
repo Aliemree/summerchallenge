0bd2e0b800e8c7050a5e2bef67e1a42e42985f4d
Ali Emre <ali.emree.tr@gmail.com>
2023-08-29 12:21:38 +0300

    write hard skill 1.week

---
+++ HardSkill_1Week/HardSkill_1Week/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace YarismaKacSaatteYakalar
+{
+    class Sıralama
+    {
+        static void Main(string[] args)
+        {
+            int ilkYarismaciHizi = 15; // km/saat
+            int ikinciYarismaciHizi = 20; // km/saat
+
+            // İki yarışmacının farklı hızlarına göre hesaplamalar yapılıyor
+            double hizFarki = ikinciYarismaciHizi - ilkYarismaciHizi;
+
+            // İlk yarışmacının yakalanma zamanı hesaplanıyor
+            double yakalamaZamani = 0;
+            if (hizFarki > 0)
+            {
+                yakalamaZamani = 15 / hizFarki; // İkilik noktada saat cinsinden yakalama zamanını hesaplıyoruz
+            }
+
+            Console.WriteLine($"İkinci yarışmacı, ilk yarışmacıyı {yakalamaZamani} saat sonra yakalar.");
+        }
+    }
+}
