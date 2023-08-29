12bf314f20910d68ece55def1763d8247939ec8f
Ali Emre <ali.emree.tr@gmail.com>
2023-08-29 12:20:05 +0300

    Write hard skiil 3 wekk

---
+++ HardSkill_3Week/HardSkill_3Week/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Skiil3
+
+{
+    class Hesaplama
+    {
+        // Faktoriyel hesaplama fonksiyonu
+        static int Faktoriyel(int n)
+        {
+            if (n <= 1)
+                return 1;
+            return n * Faktoriyel(n - 1);
+        }
+
+        // Kombinasyon hesaplama fonksiyonu
+        static int Kombinasyon(int n, int r)
+        {
+            return Faktoriyel(n) / (Faktoriyel(r) * Faktoriyel(n - r));
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 30; // Toplam öğrenci sayısı
+            int r = 4;  // Seçilecek öğrenci sayısı
+
+            int farkliSekiller = Kombinasyon(n, r);
+            Console.WriteLine($"{n} öğrenci arasından {r} farklı kişi seçme şekilleri: {farkliSekiller}");
+        }
+    }
+}
